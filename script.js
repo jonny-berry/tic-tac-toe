73f6c5c62b3546c7aac83eb4ea07ac0ecac2fce1
jonnycs <jonnydevvs@gmail.com>
2026-01-26 13:53:12 -0800

    Add gameController module and win checking functions

--- script.js
+++ script.js
@@ -1,5 +1,89 @@
+const gameController = (function () {
+  let userStats = { wins: 0, losses: 0, draws: 0 };
+  let computerStats = { wins: 0, losses: 0, draws: 0 };
+
+  function getUserStats() {
+    return userStats;
+  }
+
+  function getComputerStats() {
+    return computerStats;
+  }
+
+  function checkHorizontalWin(mark) {
+    const board = gameBoard.getBoard();
+
+    if (board[0] === mark && board[1] === mark && board[2] === mark) {
+      return true;
+    }
+    
+    if (board[3] === mark && board[4] === mark && board[5] === mark) {
+      return true;
+    }
+
+    if (board[6] === mark && board[7] === mark && board[8] === mark) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function checkVerticalWin(mark) {
+    const board = gameBoard.getBoard();
+
+    if (board[0] === mark && board[3] === mark && board[6] === mark) {
+      return true;
+    }
+    
+    if (board[1] === mark && board[4] === mark && board[7] === mark) {
+      return true;
+    }
+
+    if (board[2] === mark && board[5] === mark && board[8] === mark) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function checkDiagonalWin(mark) {
+    const board = gameBoard.getBoard();
+
+    if (board[0] === mark && board[4] === mark && board[8] === mark) {
+      return true;
+    }
+    
+    if (board[2] === mark && board[4] === mark && board[6] === mark) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function checkForWin(userMark, computerMark) {
+    if (checkHorizontalWin(userMark) || checkVerticalWin(userMark) || checkDiagonalWin(userMark)) {
+      console.log('The user won')
+      return true;
+    }
+    else if (checkHorizontalWin(computerMark) || checkVerticalWin(computerMark) || checkDiagonalWin(computerMark)) {
+      console.log('The computer won')
+      return true;
+    }
+    
+    return false;
+  }
+
+  return { checkForWin };
+})();
+
+/*
+  X X X
+  X X X
+  X X X
+*/
+
 const gameBoard = (function () {
-  let boardPieces = [ '', 'x', '', '', '', '', '', '', '' ];
+  let boardPieces = [ '', '', '', '', '', '', '', '', '' ];
 
   function getBoard() {
     return boardPieces;
@@ -13,8 +97,10 @@ const gameBoard = (function () {
 })();
 
 function createPlayer(name, mark) {
-  return { name, mark }
+  return { name, mark };
 }
 
 const user = createPlayer('Player', 'X');
 const computer = createPlayer('Computer', 'O');
+
+console.log(gameController.checkForWin(user.mark, computer.mark))
